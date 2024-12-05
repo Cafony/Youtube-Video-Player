f42fc63580dd6a8fb2bff377c437c46ca91753b9
tozec <tozec@DESKTOP-0I7P3LH>
2024-12-05 11:36:33 +0000

    Add project files.

---
+++ Youtube Video Player/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Youtube_Video_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(587, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Paste link here";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(216, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(355, 29);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Simple Youtube Video Player";
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Location = new System.Drawing.Point(100, 75);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlay.TabIndex = 7;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(100, 104);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(600, 26);
+            this.textBox1.TabIndex = 6;
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(100, 136);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(601, 325);
+            this.webBrowser1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 474);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Youtube Video Player";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ Youtube Video Player/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Youtube_Video_Player
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void LoadYouTubeVideo()
+        {
+            // Ensure the YouTube URL is valid
+            string videoUrl = textBox1.Text;
+            string videoId = GetYouTubeVideoId(videoUrl);
+
+            if (!string.IsNullOrEmpty(videoId))
+            {
+                string html = @"
+                <html>
+                <head>
+                    <meta http-equiv='X-UA-Compatible' content='IE=Edge' />
+                    <style>
+                        body, html {{
+                            margin: 0;
+                            padding: 0;
+                            overflow: hidden;
+                            height: 100%;
+                        }}
+                        iframe {{
+                            width: 100%;
+                            height: 100%;
+                            border: none;
+                        }}
+                    </style>
+                </head>
+                <body>
+                    <iframe id='video' 
+                            src='https://www.youtube.com/embed/{0}?autoplay=1' 
+                            allowfullscreen>
+                    </iframe>
+                </body>
+                </html>";
+                webBrowser1.DocumentText = string.Format(html, videoId);
+            }
+            else
+            {
+                MessageBox.Show("Invalid YouTube URL. Please provide a valid URL.");
+            }
+        }
+
+        private string GetYouTubeVideoId(string url)
+        {
+            // Extract the video ID from a standard YouTube URL
+            if (url.Contains("v="))
+            {
+                var parts = url.Split(new[] { "v=" }, StringSplitOptions.None);
+                var idPart = parts[1].Split('&')[0]; // Get the first parameter after 'v='
+                return idPart;
+            }
+
+            // Handle other formats like short URLs (e.g., youtu.be)
+            if (url.Contains("youtu.be/"))
+            {
+                var parts = url.Split(new[] { "youtu.be/" }, StringSplitOptions.None);
+                return parts[1].Split('?')[0];
+            }
+
+            return null;
+        }
+
+        private void buttonPlay_Click(object sender, EventArgs e)
+        {
+            LoadYouTubeVideo();
+
+        }
+    }
+}
